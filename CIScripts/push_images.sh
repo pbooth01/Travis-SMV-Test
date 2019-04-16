a52522c6676ca35e752c973e0fed3bcf1dae8b06
Phillip Booth <phillip.booth2015@gmail.com>
2019-04-16 17:58:19 -0400

    [major]

--- CIScripts/push_images.sh
+++ CIScripts/push_images.sh
@@ -1,16 +1,14 @@
-push_images() {
-  SEMVER_NEW_TAG=$1
-  echo $SEMVER_NEW_TAG
-  TAGGED_IMAGE=${IMAGE_NAME}:$SEMVER_NEW_TAG
-  STABLE_IMAGE=${IMAGE_NAME}:stable
-  echo ${TAGGED_IMAGE}
 
-  docker build -t $TAGGED_IMAGE .
-	docker build -t $STABLE_IMAGE .
+SEMVER_NEW_TAG=$1
+echo $SEMVER_NEW_TAG
+TAGGED_IMAGE=${IMAGE_NAME}:$SEMVER_NEW_TAG
+STABLE_IMAGE=${IMAGE_NAME}:stable
+echo ${TAGGED_IMAGE}
 
-  docker push $TAGGED_IMAGE
-  docker push $STABLE_IMAGE
-}
+docker build -t $TAGGED_IMAGE .
+docker build -t $STABLE_IMAGE .
 
-push_images
+docker push $TAGGED_IMAGE
+docker push $STABLE_IMAGE
+
 exit 0
