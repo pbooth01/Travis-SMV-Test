895f53e3980bf5c5504a8d85fbb55209a5281202
Phillip Booth <phillip.booth2015@gmail.com>
2019-04-16 17:37:28 -0400

    [major]

--- CIScripts/bump_version.sh
+++ CIScripts/bump_version.sh
@@ -6,9 +6,6 @@ bump_version(){
   export SEMVER_LAST_TAG=$(git describe --abbrev=0 --tags 2>/dev/null)
   export SEMVER_RELEASE_LEVEL=$(git log --oneline -1 --pretty=%B | cat | tr -d '\n' | cut -d "[" -f2 | cut -d "]" -f1)
 
-  echo "THE LATEST TAG IS: "
-  echo ${SEMVER_LAST_TAG}
-
   if [ -z $SEMVER_LAST_TAG ]; then
       >&2 echo "No tags defined"
       SEMVER_LAST_TAG="0.0.1"
@@ -19,8 +16,7 @@ bump_version(){
       $SEMVER_NEW_TAG=$(/tmp/semver/src/semver bump $SEMVER_RELEASE_LEVEL $SEMVER_LAST_TAG)
       git tag $SEMVER_NEW_TAG &> /dev/null
       #git push origin --tags &> /dev/null
-      echo $SEMVER_NEW_TAG
-      echo $SEMVER_NEW_TAG >> ${TRAVIS_BUILD_DIR}/persist/newTag.txt
+      echo $(($SEMVER_NEW_TAG))
   else
       >&2 echo "No release level defined"
       exit 1
