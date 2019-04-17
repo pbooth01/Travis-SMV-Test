5e91918bfa701726bff9454abfcb81f33f83ea4a
Phillip Booth <phillip.booth2015@gmail.com>
2019-04-17 12:03:00 -0400

    [major]

--- CIScripts/bump_version.sh
+++ CIScripts/bump_version.sh
@@ -3,7 +3,7 @@
 #Method to implement our semantic versioning
 #Commit messages to master must be passed in with a release level [major] [minor] [patch] or else build will fail
 bump_version(){
-  export SEMVER_LAST_TAG=$((git describe --match "[0-9].[0-9].[0-9]" --tags $(git rev-list --tags --max-count=1))>/dev/null)
+  export SEMVER_LAST_TAG=$((git describe --match "[0-9].[0-9].[0-9]" --tags $(git rev-list --tags --max-count=1))2>/dev/null)
   export SEMVER_RELEASE_LEVEL=$(git log --oneline -1 --pretty=%B | cat | tr -d '\n' | cut -d "[" -f2 | cut -d "]" -f1)
 
   if [ -z $SEMVER_LAST_TAG ]; then
